b480d9ca766e9d3935dadc67224d239125e0195a
shilpa.john <shilpa.john@ignitarium.com>
2022-09-15 12:44:15 +0000

    Add alsa package
    Former-commit-id: f6fdcaa12d1b3075e4611f31068106fe900bd499

--- requirements.sh
+++ requirements.sh
@@ -4,6 +4,7 @@ apt-get update
 apt install sudo 
 sudo apt install nodejs
 sudo apt-get install gstreamer1.0-plugins-ugly
+apt-get install gstreamer1.0-alsa
 apt-get install usbutils
 apt install udev
 apt-get install v4l-utils
