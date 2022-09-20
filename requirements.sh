b380ef9f801dc8969855dfa6a3916691a9b21253
shilpa.john <shilpa.john@ignitarium.com>
2022-09-20 18:37:15 +0000

    Remove alsa package
    Former-commit-id: f3f336697f2cd12cfd2499ebd24ad81e710bee33

--- requirements.sh
+++ requirements.sh
@@ -4,7 +4,6 @@ apt-get update
 apt install sudo 
 sudo apt install nodejs
 sudo apt-get install gstreamer1.0-plugins-ugly
-apt-get install gstreamer1.0-alsa
 apt-get install usbutils
 apt install udev
 apt-get install v4l-utils
