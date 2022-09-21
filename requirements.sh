b4359fce2d828022f24aabd0a41fa9054fcc6ead
shilpa.john <shilpa.john@ignitarium.com>
2022-09-21 06:17:09 +0000

    Add node packages
    Former-commit-id: c737b29bd3d38cab0ac9d0ee4a1bb2361115bc3a

--- requirements.sh
+++ requirements.sh
@@ -8,3 +8,7 @@ apt-get install usbutils
 apt install udev
 apt-get install v4l-utils
 pip3 install -r requirements.txt
+cd server
+npm install express --save
+npm install cors --save
+npm install log-timestamp --save
