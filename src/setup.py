c4b0f4f1e5989fb4a4d4eb3a32cdde7ecf69b5d1
shilpa.john <shilpa.john@ignitarium.com>
2022-12-21 09:03:30 +0000

    Add python script for setup process in the beginning to update config files
    Former-commit-id: 07033cbfb0df6c0e0899ce7079824e07d9676d3c

---
+++ src/setup.py
@@ -0,0 +1,25 @@
+import yaml
+import json
+import os
+from definitions import dir_path
+cwd = os.getcwd()
+ 
+config_yaml_path = ['{}{}/image_classification.yaml'.format(cwd,dir_path.CONFIG_DIR.value),'{}{}/object_detection.yaml'.format(cwd,dir_path.CONFIG_DIR.value)]
+for path in config_yaml_path:
+        
+    with open(path,'r+') as f:
+        y = json.dumps(yaml.load(f,Loader=yaml.FullLoader))
+        y=json.loads(y)
+        keyCount  = int(len(y["outputs"]))
+        
+        sink = {"output{}".format(keyCount):{"sink":"udpsink host=127.0.0.1 port=8081","width":1280,"height":720}}
+        y["outputs"].update(sink)
+        y["flows"]["flow0"]["outputs"] = ['output{}'.format(keyCount)]
+        input = {"input0":{"source":"/dev/video2","format":"jpeg","width":640,"height":360,"framerate":30}}
+        y["inputs"].update(input)
+        y["flows"]["flow0"]["input"] = "input0"
+        y["flows"]["flow0"]["mosaic"]["mosaic0"]["width"] = 640
+        y["flows"]["flow0"]["mosaic"]["mosaic0"]["height"] = 360
+        
+    with open(path,'w') as fout:
+        yaml.safe_dump(y,fout,sort_keys=False)
