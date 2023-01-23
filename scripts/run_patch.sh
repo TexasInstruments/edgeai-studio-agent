61e8cd9f85703fd7be0a9709be18fd90ad5fbbf4
shilpa.john <shilpa.john@ignitarium.com>
2023-01-23 12:31:48 +0000

    Remove scripts folder
    Former-commit-id: 5e882740058ecfc94f2c89c5b02e08535f554e77

--- scripts/run_patch.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-
-patch /opt/edge_ai_apps/apps_python/gst_wrapper.py /opt/edge_ai_apps/apps_python/ti-edgeai-studio-evm-agent-main/patch_folder/gst_wrapper_patch.txt 
-patch /opt/edge_ai_apps/apps_python/post_process.py /opt/edge_ai_apps/apps_python/ti-edgeai-studio-evm-agent-main/patch_folder/post_process_patch.txt 
